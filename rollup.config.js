40d862a8d98e5bcd45e3063e70f4dd6dd8032e7a
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2022-10-25 12:44:52 -0500

    Initial Draft (#1)
    * Initial Draft
    
    Client designed for uploading CAR files via S3

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+// @ts-ignore
+import multiInput from 'rollup-plugin-multi-input'
+
+const config = [
+  ['src', 'dist/src'],
+].map(([base, dest]) => ({
+  input: [`${base}/**/*.js`],
+  output: {
+    dir: dest,
+    preserveModules: true,
+    sourcemap: true,
+    format: 'cjs',
+    entryFileNames: '[name].cjs',
+  },
+  plugins: [multiInput({ relative: base })],
+}))
+export default config
